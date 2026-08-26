c0717dd815a11f70442f4f5f85c9de1b0c489285
LGhiglionee <lghiglione@uade.edu.ar>
2026-08-26 09:30:39 -0300

    Entidad inscripcion

--- src/main/java/com/uade/marketplace/controllers/InscripcionController.java
+++ src/main/java/com/uade/marketplace/controllers/InscripcionController.java
@@ -1,5 +1,54 @@
 package com.uade.marketplace.controllers;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.uade.marketplace.entity.Inscripcion;
+import com.uade.marketplace.entity.enums.EstadoPago;
+import com.uade.marketplace.exceptions.RecursoNoEncontradoException;
+import com.uade.marketplace.service.InscripcionService;
+
+@RestController
+@RequestMapping("/inscripciones")
 public class InscripcionController {
-    
+    @Autowired
+    private InscripcionService inscripcionService; 
+
+    @GetMapping
+    public List<Inscripcion> getAll() {
+        return inscripcionService.getAll();
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Inscripcion> getById(@PathVariable Long id) {
+    return inscripcionService.getById(id)
+            .map(ResponseEntity::ok)
+            .orElseGet(() -> ResponseEntity.notFound().build());
+    }
+
+    @PostMapping
+    public Inscripcion crear(@RequestBody Inscripcion inscripcion) {
+        return inscripcionService.crear(inscripcion);
+    }
+
+    @PutMapping("/{id}/estado-pago")
+    public Inscripcion actualizarPago(@PathVariable Long id, @RequestParam EstadoPago nuevoEstado) throws RecursoNoEncontradoException {
+        return inscripcionService.actualizarEstadoPago(id, nuevoEstado);
+    }
+
+    @DeleteMapping("/{id}")
+    public void eliminar(@PathVariable Long id) {
+        inscripcionService.eliminar(id);
+    }
 }
--- src/main/java/com/uade/marketplace/service/InscripcionService.java
+++ src/main/java/com/uade/marketplace/service/InscripcionService.java
@@ -1,5 +1,17 @@
 package com.uade.marketplace.service;
 
-public class InscripcionService {
-    
+import java.util.List;
+import java.util.Optional;
+
+import com.uade.marketplace.entity.Inscripcion;
+import com.uade.marketplace.entity.enums.EstadoPago;
+import com.uade.marketplace.exceptions.RecursoNoEncontradoException;
+
+public interface InscripcionService {
+
+    List<Inscripcion> getAll();
+    Optional<Inscripcion>getById (Long idInscripcion);
+    Inscripcion crear (Inscripcion inscripcion);
+    Inscripcion actualizarEstadoPago(Long idInscripcion, EstadoPago nuevoEstado) throws RecursoNoEncontradoException;
+    void eliminar(Long idInscripcion);
 }
